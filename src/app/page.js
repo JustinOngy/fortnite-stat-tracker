21276c1b016324638edecff1f652f6d80626b5a9
Justin Ong <justin-ong@live.com.au>
2024-02-21 21:47:02 +1100

    fetch fortnite api and display user stats

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,129 @@
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const [gamertag, setGamertag] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [playerStats, setPlayerStats] = useState({
+    wins: "",
+    kills: "",
+    matchesPlayed: "",
+    level: "",
+    username: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+    try {
+      const apiKey = "89e0abb9-96bd-4a7e-bf11-5bda8df21b1c"; // Replace with your actual API key
+      const response = await fetch(
+        `https://fortnite-api.com/v2/stats/br/v2?name=${gamertag}&accountType=epic&timeWindow=lifetime&image=none`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: apiKey,
+          },
+        }
+      );
+      const data = await response.json();
+      if (response.ok) {
+        setPlayerStats({
+          username: data.data.account.name,
+          wins: data.data.stats.all.overall.wins,
+          kills: data.data.stats.all.overall.kills,
+          matchesPlayed: data.data.stats.all.overall.matches,
+          level: data.data.battlePass.level,
+        });
+      } else {
+        setError("Failed to fetch data. Please try again.");
+      }
+    } catch (error) {
+      setError("An error occurred. Please try again.");
+      console.error("Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const mainBgStyle = {
+    backgroundImage: "url('/Fortnite-bg.jpeg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main
+      style={mainBgStyle}
+      className="flex min-h-screen flex-col items-center justify-between p-6 text-white bg-gray-900">
+      <div className="max-w-2xl w-full p-10 bg-gradient-to-br from-blue-800 to-purple-900 mt-10 mb-0 rounded-lg shadow-lg">
+        <h4 className="text-center text-lg font-bold mb-4">
+          Fortnite Player Stats
+        </h4>
+        <form onSubmit={handleSubmit} className="mt-4 flex justify-center">
+          <input
+            type="text"
+            value={gamertag}
+            onChange={(e) => setGamertag(e.target.value)}
+            className="form-input mt-1 block w-full px-3 py-2 bg-white text-black border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            placeholder="Gamertag"
+            id="gamertag"
+          />
+          <button type="submit" className="flex items-center justify-center">
+            <img
+              src="/submit.png"
+              alt="Submit"
+              className="hover:opacity-75 h-14" // Example height adjustment
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          </button>
+        </form>
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p className="text-red-500">{error}</p>
+        ) : playerStats.username ? (
+          <div className="mt-8">
+            <h4 className="text-center text-2xl font-semibold mb-4">
+              Level: {playerStats.level} | Player Name: {playerStats.username}
+            </h4>
+            <div className="flex justify-between items-center text-xl">
+              <StatDisplay
+                src="/wins.png"
+                label="Wins"
+                value={playerStats.wins}
+              />
+              <StatDisplay
+                src="/levelup.png"
+                label="Kills"
+                value={playerStats.kills}
+              />
+              <StatDisplay
+                src="/games.png"
+                label="Matches Played"
+                value={playerStats.matchesPlayed}
+              />
+              <StatDisplay
+                src="/top3.png"
+                label="Top 3"
+                value={playerStats.kills}
+              />
+            </div>
+          </div>
+        ) : null}
       </div>
     </main>
+  );
+}
+
+function StatDisplay({ src, label, value }) {
+  return (
+    <div className="text-center flex-1 flex flex-col items-center">
+      <Image src={src} alt={label} width={75} height={75} layout="fixed" />
+      <p className="font-bold">{label}</p>
+      <p>{value}</p>
+    </div>
   );
 }
